032abc404fe9050e5dbb49960f8354bb8a236269
fordarrenn <todarrenlie@gmail.com>
2026-02-02 16:32:54 +0700

    week01: (tugas 2) implement receipt printing

--- src/oop_00000110694_darren/week01/GameStore.kt
+++ src/oop_00000110694_darren/week01/GameStore.kt
@@ -6,6 +6,9 @@ fun calculateDiscount(price: Int): Int =
     } else {
         price - (price * 10/100)
     }
+fun printReceipt(title: String, finalPrice: Int){
+    println("Title: $title, Real Price: $finalPrice, Discount Price: ${calculateDiscount(finalPrice)}")
+}
 
 fun main(){
     val gameTitle = "Heartopia"
