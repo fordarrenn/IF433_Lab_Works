d9ad3240c1b6b4ba502851e7601295515b76a1b2
fordarrenn <todarrenlie@gmail.com>
2026-02-02 16:27:37 +0700

    week01: (tugas 2) implement discount logic

--- src/oop_00000110694_darren/week01/GameStore.kt
+++ src/oop_00000110694_darren/week01/GameStore.kt
@@ -1,2 +1,13 @@
 package oop_00000110694_darren.week01
 
+fun calculateDiscount(price: Int): Int =
+    if (price > 500000){
+        price - (price * 20/100)
+    } else {
+        price - (price * 10/100)
+    }
+
+fun main(){
+    val gameTitle = "Heartopia"
+    val price = 900000
+}
