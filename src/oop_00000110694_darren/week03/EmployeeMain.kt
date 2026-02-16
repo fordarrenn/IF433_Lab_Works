707be72909f89ad4a4582da47641748f3e4a0533
fordarrenn <todarrenlie@gmail.com>
2026-02-16 13:49:16 +0700

    week 03: test all encapsulation logic

--- src/oop_00000110694_darren/week03/EmployeeMain.kt
+++ src/oop_00000110694_darren/week03/EmployeeMain.kt
@@ -2,6 +2,13 @@ package oop_00000110694_darren.week03
 
 fun main(){
     val e = Employee("Budi")
-    println("===TEST ERROR===")
-    e.salary = 5000
+
+    println("===TEST===")
+    e.salary = -1000
+    e.salary = 50000
+    println("Gaji: ${e.salary}")
+
+    e.increasePerformance()
+
+    println("Pajak: ${e.tax}")
 }
