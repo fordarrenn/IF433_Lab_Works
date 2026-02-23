ca333319c6096a0f5c143cc4653e802f47e64af2
fordarrenn <todarrenlie@gmail.com>
2026-02-23 12:30:26 +0700

    week04: create Car class extending Vehicle

--- src/oop_00000110694_darren/week04/Vehicle.kt
+++ src/oop_00000110694_darren/week04/Vehicle.kt
@@ -1,2 +1,14 @@
 package oop_00000110694_darren.week04
 
+open class Vehicle(val brand: String){
+    var speed : Int = 0
+
+    open fun accelerate(){
+        speed += 10
+        println("$brand melaju. Kecepatan: $speed")
+    }
+
+    open fun honk(){
+        println("Beep beep!")
+    }
+}
