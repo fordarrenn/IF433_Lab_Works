037319fcb256e133588cc62cb6c33046fe425a23
fordarrenn <todarrenlie@gmail.com>
2026-03-09 15:07:18 +0700

    week06: (task) create SmartSpeaker

--- src/oop_00000110694_darren/week06/SmartSpeaker.kt
+++ src/oop_00000110694_darren/week06/SmartSpeaker.kt
@@ -5,6 +5,7 @@ class SmartSpeaker(override val id: String, override val name: String, val song:
         println("Speaker $name ($id) - dinyalakan")
     }
 
+    // musik
     fun playMusic(){
         println("Speaker $name ($id) memutar lagu $song")
     }
