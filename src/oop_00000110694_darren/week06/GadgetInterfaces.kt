658f2ac1365209fbf6f3c7db19a52b7ff4788ad0
fordarrenn <todarrenlie@gmail.com>
2026-03-09 13:53:04 +0700

    week06: create Camera and Phone interfaces

---
+++ src/oop_00000110694_darren/week06/GadgetInterfaces.kt
@@ -0,0 +1,13 @@
+package oop_00000110694_darren.week06
+
+interface Camera {
+    fun turnOn(){
+        println("Lensa kamera terbuka dan aktif")
+    }
+}
+
+interface Phone {
+    fun turnOn(){
+        println("Sinyal seluler mencari jaringan")
+    }
+}
