a4c8be1733fde253584464b129c782aba75e345d
fordarrenn <todarrenlie@gmail.com>
2026-03-16 16:27:46 +0700

    week 07: (task 3) test data class copy and sealed class event dispatch

--- src/oop_00000110694_darren/week07/GameMain.kt
+++ src/oop_00000110694_darren/week07/GameMain.kt
@@ -10,5 +10,11 @@ fun main(){
     val forge1 = Weapon.forgeStarterSword()
     println(forge1)
 
+    val forge2 = forge1.item.copy(damage = 25)
+    println("Upgrade: ${forge2.name} damage: ${forge2.damage}")
 
+    processEvent(BattleState.SafeZone)
+    processEvent(BattleState.MonsterEncounter("Goblin Nakal"))
+    processEvent(BattleState.LootDropped(forge2))
+    processEvent(BattleState.GameOver("Terkena jebakan racun"))
 }
