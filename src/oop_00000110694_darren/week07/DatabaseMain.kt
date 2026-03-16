832efd93c509c5566dc97d1b2b6f9382dbb208e7
fordarrenn <todarrenlie@gmail.com>
2026-03-16 14:06:50 +0700

    week 07: (trial) trigger private construct error

---
+++ src/oop_00000110694_darren/week07/DatabaseMain.kt
@@ -0,0 +1,5 @@
+package oop_00000110694_darren.week07
+
+fun main(){
+    val client = NetworkClient("http://ayam.goreng")
+}
--- src/oop_00000110694_darren/week07/DatabaseManager.kt
+++ src/oop_00000110694_darren/week07/DatabaseManager.kt
@@ -1,2 +1,10 @@
 package oop_00000110694_darren.week07
 
+object DatabaseManager {
+    var connectionStatus: String = "Disconnected"
+
+    fun connect(){
+        connectionStatus = "Connected to server"
+        println("Database is ready")
+    }
+}
