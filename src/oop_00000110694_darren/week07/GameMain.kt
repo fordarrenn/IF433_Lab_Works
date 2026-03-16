3e1acb78591208422e928e17839581dea38f8ab1
fordarrenn <todarrenlie@gmail.com>
2026-03-16 16:20:57 +0700

    week 07: (task 3) simulate rarity and factory instantiantion

--- src/oop_00000110694_darren/week07/GameMain.kt
+++ src/oop_00000110694_darren/week07/GameMain.kt
@@ -3,4 +3,12 @@ package oop_00000110694_darren.week07
 fun main(){
     GameManager.startGame()
     GameManager.startGame()
+
+    val legend = ItemRarity.LEGENDARY
+    println(legend)
+
+    val forge1 = Weapon.forgeStarterSword()
+    println(forge1)
+
+
 }
