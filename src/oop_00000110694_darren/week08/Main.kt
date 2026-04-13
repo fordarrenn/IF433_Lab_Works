c890c8fc46b372809114fc5c8540922cc6659e95
fordarrenn <todarrenlie@gmail.com>
2026-04-13 13:47:42 +0700

    week08: implement safe casting as? to filter collection

--- src/oop_00000110694_darren/week08/Main.kt
+++ src/oop_00000110694_darren/week08/Main.kt
@@ -17,11 +17,18 @@ fun main(){
     println(receipt)
 
     println("\n=== TEST SAFE CASTING ===")
-    val mixedData: List<Any> = listof(
+    val mixedData: List<Any> = listOf(
         "Smartphone",
         1500000,
         UserProfile("Andi", null)
         "Laptop",
         4500000.0
     )
+
+    for(item in mixedData){
+        val text = item as? String
+        text?.let {
+            println("Ditemukan teks: ${it.uppercase()}")
+        }
+    }
 }
