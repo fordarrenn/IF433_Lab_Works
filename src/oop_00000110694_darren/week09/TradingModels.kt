9fcb9ca177db39b1f92ea2212d38dea53bb1a82c
fordarrenn <todarrenlie@gmail.com>
2026-04-20 14:53:09 +0700

    week09: (task) create TradeLog data class

---
+++ src/oop_00000110694_darren/week09/TradingModels.kt
@@ -0,0 +1,11 @@
+package oop_00000110694_darren.week09
+
+data class TradeLog(
+    val pair: String,
+    val position: String,
+    val leverage: Int,
+    val roe: Double,
+    val status: String
+)
+
+
