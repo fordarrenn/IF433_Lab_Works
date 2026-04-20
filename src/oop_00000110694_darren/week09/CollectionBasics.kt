ad984a2406df38786f7e8a5ba0773abe664dfda1
fordarrenn <todarrenlie@gmail.com>
2026-04-20 13:45:44 +0700

    week09: implement set to discard duplicate elements

--- src/oop_00000110694_darren/week09/CollectionBasics.kt
+++ src/oop_00000110694_darren/week09/CollectionBasics.kt
@@ -1,2 +1,24 @@
 package oop_00000110694_darren.week09
 
+fun main(){
+    println("=== TEST LIST ===")
+    val framework: List<String> = listOf("Kotlin", "Java", "C++")
+
+    println("Immutable list: $framework")
+
+    val scores: MutableList<Int> = mutableListOf(85, 90)
+    scores.add(95)
+    scores[0] = 88
+    println("Mutable list: $scores")
+
+    println("\n=== TEST SET ===")
+    val uniqueNumbers = setOf(1, 2, 2, 3, 3, 4)
+    println("Unique numbers (set): $uniqueNumbers")
+
+    val activeUsers = mutableSetOf("UserA", "UserB")
+    activeUsers.add("UserC")
+    activeUsers.add("UserA")
+    println("Active users: $activeUsers")
+
+
+}
