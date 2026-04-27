5859ca6c1e758f52f1ce888e7e482c2db4e28f62
fordarrenn <todarrenlie@gmail.com>
2026-04-27 14:45:16 +0700

    week10: (task) populate coin repository

--- src/oop_00000110694_darren/week10/CryptoDashboard.kt
+++ src/oop_00000110694_darren/week10/CryptoDashboard.kt
@@ -2,4 +2,7 @@ package oop_00000110694_darren.week10
 
 fun main(){
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.add(Coin("BTC", 1.5))
+    coinRepo.add(Coin("ETH", 3.5))
+    coinRepo.add(Coin("USDT", 5.5))
 }
