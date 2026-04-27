8a73106aa4b2ba2845f9104c1bfc71a8d4d3b519
fordarrenn <todarrenlie@gmail.com>
2026-04-27 14:19:33 +0700

    week10: (task) implement search logic in repository

--- src/oop_00000110694_darren/week10/WalletRepository.kt
+++ src/oop_00000110694_darren/week10/WalletRepository.kt
@@ -2,10 +2,21 @@ package oop_00000110694_darren.week10
 
 class WalletRepository <T> (){
     private val items = mutableListOf<T>()
+
     fun add (item: T) {
         items.add(item)
     }
+
     fun getAll(): List<T> {
         return items
+    }
+
+    fun search(name: String): List<T> {
+        return items.filter{
+            when(it){
+                is Coin -> it.name.contains(name, ignoreCase = true)
+                else -> false
+            }
+        }
     }
 }
